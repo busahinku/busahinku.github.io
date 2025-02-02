4f29ec446c74cae73847add501c5ba9a0c906674
busahinku <bupacone@gmail.com>
2025-02-02 23:28:25 +0300

    bug fixes

--- app/blog/[slug]/ClientWrapper.tsx
+++ app/blog/[slug]/ClientWrapper.tsx
@@ -4,14 +4,20 @@ import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
 import type { BlogPost } from '@/app/utils/getBlogPosts';
 
+const ThemeProvider = dynamic(() => import('@/app/context/ThemeContext').then(mod => mod.ThemeProvider), {
+  ssr: false
+});
+
 const BlogPostClient = dynamic(() => import('./BlogPostClient'), {
   ssr: false
 });
 
 export default function ClientWrapper({ post }: { post: BlogPost }) {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <BlogPostClient post={post} />
-    </Suspense>
+    <ThemeProvider>
+      <Suspense fallback={<div>Loading...</div>}>
+        <BlogPostClient post={post} />
+      </Suspense>
+    </ThemeProvider>
   );
 } 
